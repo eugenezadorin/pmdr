d1d5fad4935836a6d2bcb739e35bc2782d87f5ab
Eugene Zadorin <mail@eugenezadorin.ru>
2018-07-25 17:33:00 +0300

    just code formatting

--- commands/run.js
+++ commands/run.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const settings = require('../settings.json');
-const Pomodoro =require('../lib/Pomodoro.js');
+const Pomodoro = require('../lib/Pomodoro.js');
 
 module.exports = function(task) {
 	const pmdr = new Pomodoro(task, settings);
