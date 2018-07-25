ea36ab3c34bc9d009c0507f91ededf418c12a2e2
Eugene Zadorin <mail@eugenezadorin.ru>
2018-07-25 17:33:22 +0300

    show today stats

--- commands/stat.js
+++ commands/stat.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const StatsManager = require('../lib/Stats.js');
+
 module.exports = function(date) {
-	console.log('stats for ' + date);
+	const manager = new StatsManager();
+	if (!date) {
+		manager.init(manager.todayStatsFile, (stats) => {
+			console.log('Pomodoro - today stats');
+			console.log('');
+			for (let i in stats) {
+				console.log(i + ': ' + stats[i]);
+			}
+		});
+	}
 };
